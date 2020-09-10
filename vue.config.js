b08a407eb19d529a524a0195e4ac2695806e9e2c
郝先瑞 <6567776+haoxianrui@user.noreply.gitee.com>
2020-09-11 01:20:53 +0800

    初始导入

--- vue.config.js
+++ vue.config.js
@@ -28,6 +28,7 @@ module.exports = {
   outputDir: 'dist',
   assetsDir: 'static',
   // lintOnSave: process.env.NODE_ENV === 'development',
+  lintOnSave: false,
   productionSourceMap: false,
   devServer: {
     port: port,
