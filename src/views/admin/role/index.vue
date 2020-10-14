9121e75b5a9f9080586b84956df49cd94c19c07d
haoxr <1490493387@qq.com>
2020-10-15 06:43:29 +0800

    feat:资源分配

--- src/views/admin/role/index.vue
+++ src/views/admin/role/index.vue
@@ -22,11 +22,11 @@
     </el-form>
 
     <el-table v-loading="loading" :data="pageList" border @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="角色编号" prop="id" width="280" />
-      <el-table-column label="角色名称" prop="name" :show-overflow-tooltip="true" width="200" />
-      <el-table-column label="权限字符" prop="perms" :show-overflow-tooltip="true" width="200" />
-      <el-table-column label="显示顺序" prop="sort" width="100" />
+      <el-table-column type="selection" width="55" align="center"/>
+      <el-table-column label="角色编号" prop="id" width="280"/>
+      <el-table-column label="角色名称" prop="name" :show-overflow-tooltip="true" width="200"/>
+      <el-table-column label="权限字符" prop="perms" :show-overflow-tooltip="true" width="200"/>
+      <el-table-column label="显示顺序" prop="sort" width="100"/>
       <el-table-column label="状态" align="center" width="100">
         <template slot-scope="scope">
           <el-switch
@@ -37,9 +37,16 @@
           />
         </template>
       </el-table-column>
-      <el-table-column label="备注" prop="remark" />
-      <el-table-column label="操作" align="center" width="180" class-name="small-padding fixed-width">
+      <el-table-column label="备注" prop="remark"/>
+      <el-table-column label="操作" align="center" width="280" class-name="small-padding fixed-width">
         <template slot-scope="scope">
+          <el-button
+            type="text"
+            icon="el-icon-edit"
+            @click="handleAllocateResource(scope.row)"
+          >分配资源
+          </el-button>
+
           <el-button
             type="text"
             icon="el-icon-edit"
@@ -64,17 +71,17 @@
       @pagination="handleQuery"
     />
 
-    <!-- 添加或修改角色配置对话框 -->
+    <!-- 角色弹窗 -->
     <el-dialog :title="dialog.title" :visible.sync="dialog.visible" width="800px">
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
         <el-form-item label="角色名称" prop="name">
-          <el-input v-model="form.name" placeholder="请输入角色名称" />
+          <el-input v-model="form.name" placeholder="请输入角色名称"/>
         </el-form-item>
         <el-form-item label="权限字符" prop="perms">
-          <el-input v-model="form.perms" placeholder="请输入权限字符" />
+          <el-input v-model="form.perms" placeholder="请输入权限字符"/>
         </el-form-item>
         <el-form-item label="角色顺序" prop="sort">
-          <el-input-number v-model="form.sort" controls-position="right" :min="0" />
+          <el-input-number v-model="form.sort" controls-position="right" :min="0"/>
         </el-form-item>
         <el-form-item label="状态">
           <el-radio-group v-model="form.status">
@@ -92,7 +99,7 @@
           />
         </el-form-item>
         <el-form-item label="备注">
-          <el-input v-model="form.remark" type="textarea" placeholder="请输入内容" />
+          <el-input v-model="form.remark" type="textarea" placeholder="请输入内容"/>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -100,12 +107,36 @@
         <el-button @click="dialog.visible=false">取 消</el-button>
       </div>
     </el-dialog>
+
+
+    <!-- 分配资源弹窗 -->
+    <el-dialog title="分配资源" :visible.sync="resourceDialog.visible" width="800px">
+      <el-form ref="resourceForm" :model="resourceForm" :rules="resourceRule" label-width="80px">
+        <el-form-item label="角色名称" prop="name">
+          <el-input v-model="resourceForm.name" :readonly="true"/>
+        </el-form-item>
+        <el-form-item label="资源">
+          <el-tree
+            ref="resource"
+            :data="resourceOptions"
+            show-checkbox
+            node-key="id"
+            empty-text="加载中，请稍后"
+          />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="handleAllocateResourceSubmit">确 定</el-button>
+        <el-button @click="resourceDialog.visible=false">取 消</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { list, detail, update, add, del, patch } from '@/api/admin/role'
-import { list as menuList } from '@/api/admin/menu'
+import {list, detail, update, add, del, patch} from '@/api/admin/role'
+import {list as menuList} from '@/api/admin/menu'
+import {list as resourceList} from '@/api/admin/resource'
 
 export default {
   data() {
@@ -138,12 +169,22 @@ export default {
       // 表单校验
       rules: {
         name: [
-          { required: true, message: '角色名称不能为空', trigger: 'blur' }
+          {required: true, message: '角色名称不能为空', trigger: 'blur'}
         ],
         perms: [
-          { required: true, message: '权限字符不能为空', trigger: 'blur' }
+          {required: true, message: '权限字符不能为空', trigger: 'blur'}
         ]
-      }
+      },
+      resourceDialog: {
+        visible: false
+      },
+      resourceForm: {
+        id: undefined,
+        name: undefined,
+        resourceIds: []
+      },
+      resourceRule: {},
+      resourceOptions: []
     }
   },
   created() {
@@ -181,11 +222,11 @@ export default {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
-      }).then(function() {
-        return patch(row.id, { status: row.status })
+      }).then(function () {
+        return patch(row.id, {status: row.status})
       }).then(() => {
         this.$message.success(text + '成功')
-      }).catch(function() {
+      }).catch(function () {
         row.status = row.status === 1 ? 0 : 1
       })
     },
@@ -224,7 +265,7 @@ export default {
         this.$message.info('已取消删除')
       )
     },
-    handleSubmit: function() {
+    handleSubmit: function () {
       this.$refs['form'].validate(valid => {
         if (valid) {
           const id = this.form.id
@@ -247,8 +288,8 @@ export default {
       })
     },
     resetForm() {
-      if (this.$refs.tree != undefined) {
-        this.$refs.tree.setCheckedKeys([])
+      if (this.$refs.menu != undefined) {
+        this.$refs.menu.setCheckedKeys([])
       }
       this.form = {
         id: undefined,
@@ -264,12 +305,12 @@ export default {
       }
     },
     loadMenuOptions() {
-      menuList({ mode: 2 }).then(response => {
+      menuList({mode: 2}).then(response => {
         this.menuOptions = response.data
       })
     },
     loadRoleMenuOptions(roleId) {
-      menuList({ mode: 2, roleId: roleId }).then(response => {
+      menuList({mode: 2, roleId: roleId}).then(response => {
         this.menuOptions = response.data.menus
         this.$refs.menu.setCheckedKeys(response.data.checkedKeys)
       })
@@ -282,6 +323,40 @@ export default {
       const checkedKeys = this.$refs.menu.getCheckedKeys()
       //checkedKeys.unshift.apply(checkedKeys, halfCheckedKeys)
       return checkedKeys
+    },
+    // 分配资源
+    async handleAllocateResource(row) {
+      this.resetResourceForm()
+      this.resourceDialog.visible = true
+      const roleId = row.id
+      this.resourceForm.id = roleId
+      this.resourceForm.name = row.name
+      await this.loadRoleResourceOptions(roleId)
+    },
+    handleAllocateResourceSubmit: function () {
+      this.$refs['resourceForm'].validate(valid => {
+        if (valid) {
+        }
+      })
+    },
+    resetResourceForm: function () {
+      if (this.$refs.resource != undefined) {
+        this.$refs.resource.setCheckedKeys([])
+      }
+      this.resourceForm = {
+        id: undefined,
+        name: undefined,
+        resourceIds: []
+      }
+      if (this.$refs['resourceForm']) {
+        this.$refs['resourceForm'].resetFields()
+      }
+    },
+    loadRoleResourceOptions(roleId) {
+      resourceList({mode: 2, roleId: roleId}).then(response => {
+        this.resourceOptions = response.data.resources
+        this.$refs.menu.setCheckedKeys(response.data.checkedKeys)
+      })
     }
   }
 }
