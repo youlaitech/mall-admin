70a1dcc810531fb85d07904e9a5ec3e00cf65368
haoxr <1490493387@qq.com>
2021-01-11 00:09:34 +0800

    feat:权限管理页面

---
+++ src/api/admin/permission.js
@@ -0,0 +1,48 @@
+import request from '@/utils/request'
+
+export function list(queryParams) {
+  return request({
+    url: '/youlai-admin/app.admin/v1/permissions',
+    method: 'get',
+    params: queryParams
+  })
+}
+
+export function detail(id) {
+  return request({
+    url: '/youlai-admin/app.admin/v1/permissions/' + id,
+    method: 'get'
+  })
+}
+
+export function add(data) {
+  return request({
+    url: '/youlai-admin/app.admin/v1/permissions',
+    method: 'post',
+    data: data
+  })
+}
+
+export function update(id, data) {
+  return request({
+    url: '/youlai-admin/app.admin/v1/permissions/' + id,
+    method: 'put',
+    data: data
+  })
+}
+
+export function del(ids) {
+  return request({
+    url: '/youlai-admin/app.admin/v1/permissions',
+    method: 'delete',
+    params: { ids: ids }
+  })
+}
+
+export function patch(id, data) {
+  return request({
+    url: '/youlai-admin/app.admin/v1/permissions/' + id,
+    method: 'patch',
+    data: data
+  })
+}
