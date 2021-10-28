0b76d3365a5a890ed6cca0c1316041b8b1dcafaf
有来技术 <1490493387@qq.com>
2021-10-29 00:38:03 +0800

    refactor(views/role): 优化角色权限设置界面

--- src/views/system/role/index.vue
+++ src/views/system/role/index.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="app-container">
     <el-row :gutter="10">
-      <el-col :span="10" :xs="24">
+      <el-col :span="12" :xs="24">
         <role ref="role" @roleClick="roleClick"></role>
       </el-col>
-      <el-col :span="6" :xs="24">
+      <el-col :span="12" :xs="24">
         <menus ref="menu" @menuClick="menuClick"></menus>
-      </el-col>
-      <el-col :span="8" :xs="24">
-        <permission ref="permission"></permission>
       </el-col>
     </el-row>
   </div>
@@ -18,11 +15,10 @@
 <script>
 import Role from './components/Role'
 import Menus from './components/Menu'
-import Permission from './components/Permission'
 
 export default {
   name: "index",
-  components: {Permission, Role, Menus},
+  components: { Role, Menus},
   data() {
     return {
       role: undefined,
@@ -34,7 +30,6 @@ export default {
       this.role = role
       this.menu = undefined
       this.$refs.menu.roleClick(role)
-      this.$refs.permission.menuClick(this.menu, this.role)
     },
     menuClick(menu) {
       if (!this.role) {
@@ -42,7 +37,6 @@ export default {
         return
       }
       this.menu = menu
-      this.$refs.permission.menuClick(this.menu, this.role)
     }
   }
 }
