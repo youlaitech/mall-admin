0a786511aaa2a70b344b13af339530d4d3e73bd4
郝先瑞 <1490493387@qq.com>
2022-04-15 23:40:13 +0800

    feat(.eslintrc.js): 添加eslint代码检查配置文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,7 +26,7 @@ module.exports = {
     ],
     "rules": {
         "vue/multi-word-component-names": "off",
-        "@typescript-eslint/no-empty-function": "off", // 关闭空方法
+        "@typescript-eslint/no-empty-function": "off", // 关闭空方法检查
         "@typescript-eslint/no-explicit-any": "off", // 关闭any类型的警告
         "vue/no-v-model-argument": "off"
     }
