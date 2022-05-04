cd677176f0fc1d28aed7c238b4d951075cfd80e8
郝先瑞 <1490493387@qq.com>
2022-05-04 13:42:09 +0800

    fix(.prettierrc.js): 删除已废弃的配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -20,8 +20,6 @@ module.exports = {
 	trailingComma: 'es5',
 	// 在对象文字中的括号之间打印空格
 	bracketSpacing: true,
-	// jsx 标签的反尖括号需要换行
-	jsxBracketSameLine: false,
 	// 在单独的箭头函数参数周围包括括号 always：(x) => x \ avoid：x => x
 	arrowParens: 'always',
 	// 这两个选项可用于格式化以给定字符偏移量（分别包括和不包括）开始和结束的代码
