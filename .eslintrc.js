8941e80edbecc2ad509c71cde842e694ec402287
郝先瑞 <1490493387@qq.com>
2022-07-28 23:55:16 +0800

    docs: 关闭空方法检查

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,9 +23,9 @@ module.exports = {
   plugins: ['vue', '@typescript-eslint'],
   rules: {
     'vue/multi-word-component-names': 'off',
-    "no-empty": 'off', // 关闭空方法检查
+    '@typescript-eslint/no-empty-function': 'off', // 关闭空方法检查
     '@typescript-eslint/no-explicit-any': 'off', // 关闭any类型的警告
     'vue/no-v-model-argument': 'off',
-
+    "no-empty": 'off', // 关闭空方法检查
   },
 };
