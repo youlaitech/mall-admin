2ec786cf80a2d263ece5a55d2a6ba15960bfd954
haoxr <1490493387@qq.com>
2022-11-20 22:43:20 +0800

    feat: 文件API添加ts类型声明

---
+++ src/api/file/index.ts
@@ -0,0 +1,34 @@
+import request from '@/utils/request';
+import { AxiosPromise } from 'axios';
+import { FileInfo } from './types';
+
+/**
+ * 上传文件
+ *
+ * @param file
+ */
+export function uploadFileApi(file: File): AxiosPromise<FileInfo> {
+  const formData = new FormData();
+  formData.append('file', file);
+  return request({
+    url: '/youlai-system/api/v1/files',
+    method: 'post',
+    data: formData,
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+}
+
+/**
+ * 删除文件
+ *
+ * @param fileName 文件名
+ */
+export function deleteFileApi(fileName?: string) {
+  return request({
+    url: '/youlai-system/api/v1/files',
+    method: 'delete',
+    params: { fileName: fileName },
+  });
+}
